b527188aa32164ffbef8d23353e1a7c90a00ea1e
Sajib Hossain <sajib.sj359@gmail.com>
2022-05-18 18:47:30 +0600

    added booking modal

--- src/Pages/Appointment/AvailableAppointment.js
+++ src/Pages/Appointment/AvailableAppointment.js
@@ -1,9 +1,11 @@
 import { format } from 'date-fns';
 import React, { useEffect, useState } from 'react';
+import BookingModal from './BookingModal';
 import Service from './Service';
 
 const AvailableAppointment = ({date}) => {
     const [services ,setServices] = useState([])
+    const [treatment , setTreatment] = useState(null)
     useEffect(()=>{
         fetch('services.json')
         .then(res =>res.json())
@@ -18,8 +20,16 @@ const AvailableAppointment = ({date}) => {
                     services.map(service=><Service
                     key={service._id}
                     service={service}
+                    setTreatment={setTreatment}
                     ></Service>)
                 }
+            </div>
+            <div>
+                {treatment && <BookingModal
+                 treatment={treatment} 
+                 date={date}
+                 setTreatment={setTreatment}
+                 ></BookingModal>} 
             </div>
         </div>
     );
--- src/Pages/Appointment/Service.js
+++ src/Pages/Appointment/Service.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Service = ({ service }) => {
+const Service = ({ service ,setTreatment}) => {
     const {name ,slots} = service;
     return (
         <div class="card lg:max-w-lg bg-base-100 shadow-xl">
@@ -15,7 +15,10 @@ const Service = ({ service }) => {
                 </p>
                 <p className='text-center'>{slots.length} {slots.length > 1 ? 'spaces' : 'space' } are available</p>
                 <div class="card-actions justify-center">
-                    <button disabled ={slots.length === 0} class="btn btn-secondary text-white uppercase">Booked Appointment</button>
+                    <label for="booking-modal" 
+                     disabled ={slots.length === 0}
+                     onClick={()=> setTreatment (service)}
+                    class="btn btn-secondary text-white uppercase">Booked Appointment</label>
                 </div>
             </div>
         </div>
