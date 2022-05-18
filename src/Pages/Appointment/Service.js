de2191e7cad60fda36645f188a2aa208fc08ad42
Sajib Hossain <sajib.sj359@gmail.com>
2022-05-18 14:28:19 +0600

    added Booking section op appoinment

--- src/Pages/Appointment/Service.js
+++ src/Pages/Appointment/Service.js
@@ -5,10 +5,17 @@ const Service = ({ service }) => {
     return (
         <div class="card lg:max-w-lg bg-base-100 shadow-xl">
             <div class="card-body">
-                <h2 class="card-title">{name}</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div class="card-actions justify-end">
-                    <button class="btn btn-primary">Buy Now</button>
+                <h2 className="card-title text-secondary text-center">{name}</h2>
+                <p className='text-center'>
+                    {
+                        slots.length > 0
+                        ? <span>{slots[0]}</span>
+                        :<span className='text-red font-bold '>No Slots Available</span>
+                    }
+                </p>
+                <p className='text-center'>{slots.length} {slots.length > 1 ? 'spaces' : 'space' } are available</p>
+                <div class="card-actions justify-center">
+                    <button disabled ={slots.length === 0} class="btn btn-secondary text-white uppercase">Booked Appointment</button>
                 </div>
             </div>
         </div>
