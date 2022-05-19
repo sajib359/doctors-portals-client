6b816005f54415a9b311b06042bba5d703fbbc97
Sajib Hossain <sajib.sj359@gmail.com>
2022-05-19 15:02:42 +0600

    login

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import About from './Pages/About/About';
 import Appointment from './Pages/Appointment/Appointment';
 import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
 import Navbar from './Pages/Shared/Navbar';
 
 function App() {
@@ -15,7 +16,7 @@ function App() {
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/about' element={<About></About>}></Route>
         <Route path='/appointment' element={<Appointment></Appointment>}></Route>
-        <Route path='/login' element={<About></About>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
       </Routes>
     </div>
   );
