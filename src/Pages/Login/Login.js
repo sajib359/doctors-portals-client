475db04d0b11287ade4430ab4c0bc43126f7bacc
Sajib Hossain <sajib.sj359@gmail.com>
2022-05-19 18:30:55 +0600

    added login form

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,16 @@
 import React from "react";
 import { useSignInWithGoogle } from "react-firebase-hooks/auth";
 import auth from "../../firebase.init";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
+  const onSubmit = (data) => console.log(data);
   const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
 
   if (user) {
     console.log(user);
@@ -13,6 +20,35 @@ const Login = () => {
       <div className="card w-96 bg-base-100 shadow-xl">
         <div className="card-body">
           <h2 className="text-center font-bold text-2xl">Login !</h2>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div class="form-control w-full max-w-xs">
+              <label class="label">
+                <span class="label-text">Email</span>
+                
+              </label>
+              <input
+                type="email"
+                placeholder="Enter Your Email"
+                class="input input-bordered w-full max-w-xs"
+                {...register("email", { pattern:{
+                    value: /[A-Za-z]{3}/,
+                    message: 'error message' 
+                } 
+            })} 
+              />
+              <label class="label">
+                <span class="label-text-alt">Alt label</span>
+                
+              </label>
+            </div>
+            <input {...register("firstName", { required: true })} />
+            {errors.firstName?.type === "required" && "First name is required"}
+
+            <input {...register("lastName", { required: true })} />
+            {errors.lastName && "Last name is required"}
+
+            <input type="submit" />
+          </form>
 
           <div className="divider">OR</div>
 
